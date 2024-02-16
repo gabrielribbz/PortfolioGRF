f9b0066656e7807fa2e89072a4135ec820367933
Gabriel Ribeiro <144161116+gabrielribbz@users.noreply.github.com>
2024-02-16 20:44:49 -0300

    second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,56 @@
-const menu = document.querySelector(".menu")
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100 )
+
+    menu.classList.remove('open');
+    navbar.classList.remove('open');
+};
+
+
+let menu = document.querySelector(".menu")
+let navbar = document.querySelector('.navbar')
+
 
 menu.addEventListener('click', () => {
-    menu.classList.toggle('open')
+    menu.classList.toggle('open');
+    navbar.classList.toggle('open');
 })
 
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Sou Frontend Developer','Faço Programa','Sou Apostador'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+})
